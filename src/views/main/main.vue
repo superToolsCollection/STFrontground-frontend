056d4e8a815ed08d90e0734c203a4ea6db250e5e
百事百闻 <52311345+fishWhy@users.noreply.github.com>
2021-01-03 22:06:21 +0800

    1

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container">
-        <div class="head"></div>
+        <div class="head">
+            <div class="logo"><img src="~assets/img/sigin/tab.png" alt=""><span>superTools</span></div>
+            <div class="title">
+                <ul>
+                    <li>首页</li>
+                    <li>superTools</li>
+                    <li>服务范围</li>
+                    <li>资讯中心</li>
+                    <li>关于</li>
+                    <li>用户</li>
+                </ul>
+            </div>
+        </div>
         <div class="leftMenu" :class="{'leftPushMenu': showMenu}">
             <menu-left></menu-left>
         </div>
@@ -13,6 +25,9 @@
 <script>
 import menuLeft from './childComps/menu.vue'
 import mnImg from 'assets/img/main/menu.svg'
+
+// import bgImg from 'assets/img/background.png'
+
 export default{
     components:{
         menuLeft
@@ -38,24 +53,44 @@ export default{
 @import "scss-normalize";
 
 .container{
+    background-image: url('~assets/img/background.png');
+    // background-color: white;
     // border: 1px solid black;
     position: relative;
     font-size: 0;
     // padding: 0;
     .head{
         border: 1px solid red;
-        height: vh(51);
+        height: vh(77);
         width: 100%;
+        .logo{
+            float: left;
+            margin-left: 3%;
+            margin-top: 0.98%;
+            border: 1px solid blue;
+            height: 52%;
+            width: 10%;
+            img{
+                height: 100%;
+
+            }
+
+        }
         box-sizing: border-box;
+        .title{
+            ul>li{
+                color: white;
+            }
+        }
     }
 
     .leftMenu{
         z-index: 2;
         position: absolute;
-        top: vh(51);
+        top: vh(77);
         left: vw(-374);
         width: vw(374);
-        height: vh(945);
+        height: vh(849);
 
         transition: left 1s ease; 
         box-sizing: border-box;
@@ -70,7 +105,7 @@ export default{
         box-sizing: border-box;
         border: 1px solid red;
         width: 100%;
-        height: vh(945);
+        height: vh(849);
         // 触发BFC
         overflow: hidden;
         img{
