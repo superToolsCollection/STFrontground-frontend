bb645e979c88adfdcbe8e3df23ea33e3c54b80e8
百事百闻 <52311345+fishWhy@users.noreply.github.com>
2021-01-06 22:20:36 +0800

    main

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,42 +1,29 @@
 <template>
     <div class="containerMain">
         <div class="head">
-            <div class="logo"><img src="~assets/img/sigin/tab.png" alt=""><span>superTools</span></div>
+            <div class="logo"><img src="~assets/img/main/logo.svg" alt=""><span>superTools</span></div>
             <div class="title">
+                
                 <ul>
                     <li>首页</li>
-                    <li>superTools</li>
-                    <li>服务范围</li>
-                    <li>资讯中心</li>
-                    <li>关于</li>
+                    <li>工具主页</li>
+                    <li>关于我们</li>
                     <li>用户</li>
                 </ul>
+                <div class="search" @click="leftPushMenu">
+                    <img :src="searchTitle.img" alt=""><span>{{searchTitle.title}}</span>
+                </div>                
             </div>
         </div>
-        
-        <div class="main" :style="{height: 260+height+'px'}">
-            <div class="leftMenu" :class="{'leftPushMenu': showMenu}">
-                <menu-left></menu-left>
+        <div class="main">
+            <div class="rightMenu" :class="{'rightPushMenu': showSearchMenu}">
+                <menu-right></menu-right>
             </div>
-            <img :src="menuImg" alt="" :class="{'leftPushMenuImg': showMenu}" @click="leftPushMenu">
-            <div class="search">
-                <input type="text">
-            </div>
-            <div class="content" :style="{height: 50+height+'px'}">
-                <div class="contentTitle">
-                    <ul>
-                        <li>全部</li>
-                        <li>生活日常</li>
-                        <li>辅助办公</li>
-                        <li>教育学习</li>
-                        <li>图片视频</li>
-                        <li>程序开发</li>
-                    </ul>
-                </div>
-                <span>{{tab}}</span>
-                <div class="tools" :style="{height: height+'px'}">
-                    <content-main :list="showContent"></content-main>
-                    <div class="showMore" @click="showMore">加载更多</div>
+            <div class="content">
+                <div class="tools" >
+                    <div class="area" v-for="(it, index) in list"  :key="index">
+                         <content-main :content=it></content-main>
+                    </div>                  
                 </div>
             </div>
         </div>    
@@ -44,61 +31,54 @@
 </template>
 
 <script>
-import mnImg from 'assets/img/main/menu.svg'
+import menuRight from './childComps/menu.vue'
+import contentMain from './childComps/toolsPage/content.vue'
 
-import menuLeft from './childComps/menu.vue'
-import contentMain from './childComps/content.vue'
+import {mainData} from 'assets/js/data.js' 
 
-import {mainItem} from 'assets/js/data.js' 
+import searchImg from 'assets/img/main/search.svg'
+import cancleImg from 'assets/img/main/cancle.svg'
 
 // import bgImg from 'assets/img/background.png'
 
 export default{
     components:{
-        menuLeft,
+        menuRight,
         contentMain
     },
     data(){
        return {
-           menuImg: mnImg,
-
-           showMenu: false,
+           showSearchMenu: false,
+           searchTitle:{
+               img: searchImg,
+               title: "搜索"
+           },
 
            tab: "生活日常",
 
-           list: mainItem,
+           list: mainData,
 
-           showLength: 16,
-
-           height: 4*145,
+           
 
         }
     },
     computed:{
-        showContent:function () {
-            // console.log(this.list.length)
-
-            if(this.list.length>this.showLength){
-                return this.list.slice(0,this.showLength);
-            }else {
-                return this.list;
-            }    
-        },
+        
     },
     methods:{
         leftPushMenu: function(){
-            this.showMenu = !this.showMenu;
-            console.log(this.showMenu);
-        },
-        showMore: function(){
-            console.log(this.list.length)
-            if(this.showLength+16<=this.list.length){
-                this.showLength = this.showLength + 16;
-            } else if( this.showLength != this.list.length){
-                this.showLength = this.list.length
+            if(this.showSearchMenu){
+                this.searchTitle.img = searchImg;
+                this.searchTitle.title = '搜索';              
+            }else{
+                this.searchTitle.img = cancleImg;
+                this.searchTitle.title = '取消';
             }
-            this.height =  this.showLength/4*145
-        }
+            this.showSearchMenu = !this.showSearchMenu;
+            console.log(this.showSearchMenu);
+        },
+        
+        
     }
 }
 </script>
@@ -111,12 +91,25 @@ export default{
     position: relative;
     // font-size: 0;
     // padding: 0;
+    height: 100%;
+
+    //禁止鼠标多次点击选中div中的文字，参考：https://blog.csdn.net/xiliuhu/article/details/6885749
+    -moz-user-select: none;
+    -khtml-user-select: none;
+    user-select: none;
+    
     .head{
-        position: relative;
-        border: 1px solid red;
+        position: fixed;
+        top: 0;
+        // border: 1px solid red;
         height: vh(77);
         width: 100%;
         box-sizing: border-box;
+        background-color: white;
+        // background-color: rgba(#fff,#fff,#fff);
+        opacity: 1;
+        z-index: 3;
+        border-bottom: 1px solid #f0f0f4;
         .logo{
             float: left;
             position: absolute;
@@ -125,7 +118,7 @@ export default{
             bottom: 0;
             margin-top: auto;
             margin-bottom: auto;
-            border: 1px solid blue;
+            // border: 1px solid blue;
             height: vh(42);
             text-align: center;
             img{
@@ -138,43 +131,73 @@ export default{
                 line-height: vh(42);
                 font-size: vh(35);
                 font-weight: 600;
+                color: #50616d;
             }
         }
         
         .title{
             float: right;
             position: absolute;
-            right: vw(130);
+            // height = calc(percent - vh(value)) 
+            @include setAttribute('&',left,100%,500);
+            // margin-right: vw(130);
             top: 0;
             bottom: 0;
             margin-top: auto;
             margin-bottom: auto;
             // border: 1px solid blue;
-            height: vh(42);
+            height: vh(23);
 
             // @include clearFloat;
             ul{
+                float: left;
                 // background-color: green;
                 font-size: 0;
                 text-align: center;
-                display: block;
+                // display: block;
                 // border: 1px solid red;
                 list-style-type: none;
                 padding: 0;
 
                 li{
                     display: inline-block;
-                    width: vw(74);
+                    // width: vw(74);
                     // display: inline;
                     list-style-type: none;
                     // border: 1px solid red;
-                    // color: ;
-                    line-height: vh(42);
-                    font-size: vh(15);
-                    font-weight:normal;
+                    box-sizing: border-box;
+                    color: #696969;
+                    line-height: vh(23);
+                    font-size: vh(18);
+                    font-weight: normal;
+                    margin-right: vh(40);
+                    @include hoverCursor;
                 }
-                li:not(:first-child){
-                    margin-left: vw(70);
+                // li:not(:first-child){
+                //     margin-left: vw(40);
+                // }
+            }
+            .search{
+                float: left;
+                box-sizing: border-box;
+                @include hoverCursor;
+
+                // border: 1px solid red;
+                // display: inline-block;
+                img{
+                    width: vh(23);
+                    height: vh(23);
+                    // border: 1px solid green;
+                    box-sizing: border-box;
+                    float: left;
+                    // color: #696969;
+                }
+                span{
+                    color: #696969;
+                    line-height: vh(23);
+                    font-size: vh(18);
+                    font-weight: normal;
+                    float: left;
                 }
             }
         }
@@ -185,147 +208,70 @@ export default{
         position: relative;
         left: vw(0);
         box-sizing: border-box;
-        border: 1px solid yellow;
+        // border: 1px solid yellow;
         width: 100%;
-        height: vh(810);
+        margin-top: vh(77);
+        // margin-bottom: vh(80);
+        // min-height: vh(840);
+        // height: 100%;
+
+        // height: 100%-vh(77);
+        // @include changeHeight(&,77);
+        
         // 触发BFC
         // overflow: hidden;
-        .leftMenu{
-            z-index: 2;
-            position: absolute;
-            top: 0;
-            left: vw(-374);
+        .rightMenu{
+            position: fixed;
+            z-index: 3;
+            top: vh(77);
+
+            right: vw(-374);
+            // right: 0;
             width: vw(374);
-            height: 100%;
-            transition: left 0.5s ease; 
+            // height = calc(percent - vh(value)) 
+            @include setAttribute('&',height,100%,77);
+            // height: 100%;
+            transition: right 0.5s ease; 
             box-sizing: border-box;
             border: 1px solid red;
         }
-        .leftPushMenu{
-            left: 0;
-        }
-
-        img{
-            position: absolute;
-            left: vw(84);
-            top: vh(43);
-            width: vw(24);
-            height: vh(24);
-
-            transition: left 0.5s ease; 
-
-        }
-        .leftPushMenuImg{
-            left: vw(458);
-        }
-
-        .search{
-            position: absolute;
-            left: 0;
+        .rightPushMenu{
             right: 0;
-            top: vh(100);
-            margin-left: auto;
-            margin-right: auto;
-            width: vw(950);
-            height: vh(50);
-            // border: 1px solid red;
-            box-sizing: border-box;
-            input{
-                width: 100%;
-                height: 100%;
-                box-sizing: border-box;
-            }
         }
+
+
 
         .content{
-            position: absolute;
-            left: 0;
-            right: 0;
-            top: vh(180);
+            position: relative;
             margin-left: auto;
             margin-right: auto;
             width:vw(1235);
-            height: vh(630);
-            border: 1px solid blue;
-            .contentTitle{
-                position: absolute;
-                right: vw(0);
-                // border: 1px solid blue;
-                top: vh(12);
-                height: vh(20);
-                // @include clearFloat;
-                ul{
-                    // background-color: green;
-                    font-size: 0;
-                    text-align: center;
-                    display: block;
-                    // border: 1px solid red;
-                    list-style-type: none;
-                    padding: 0;
-                    li{
-                        display: inline-block;
-                        width: vw(74);
-                        // display: inline;
-                        list-style-type: none;
-                        // color: white;
-                        line-height: vh(20);
-                        font-size: vh(15);
-                        font-weight:normal;
-                    }
-                    li:not(:first-child){
-                        margin-left: vw(35);
-                    }
-                }
-            }
-            
-            span{
-                display: block;
-                position: absolute;
-                top: vh(-4);
-                margin-top: 0;
-                padding: 0;
-                left: vw(18);
-                font-size: vh(25);
-                font-weight: 720;
-
-            }
+            // 内部进行：height: 100% - vh(180);
+            // @include changeHeight('&',208);
+            // border: 1px solid blue;
             .tools{
-                position: absolute;
-                left: 0;
-                right: 0;
-                top: vh(50);
-                margin-left: auto;
-                margin-right: auto;
-                border: 1px solid red;
+                position: relative;
+                top: vh(60);
+                // left: 0;
+                // right: 0;
+                // top: 0;
+                // margin-left: auto;
+                // margin-right: auto;
+                // margin-bottom: vh(50);
+                // border: 1px solid red;
                 box-sizing: border-box;
                 width:vw(1250);
-                height: vh(580);
+                // height: vh(580);
+                // @include changeHeight(&,50);
                 // overflow: hidden;
-                .showMore{
-                    position: absolute;
-                    left: 0;
-                    right: 0;
-                    margin-left: auto;
-                    margin-right: auto;
-                    bottom: 0;
-                    width: vw(80);
-                    height: vh(30);
-                    // border: 1px solid red;
-                    // color: red;
-                    color: #ECF0F1;
-                    background-color: #E74C3C;
-
-                    font-size: vh(15);
-                    line-height: vh(30);
-                    text-align: center;
-                    // vertical-align: center;
-                    border-radius: 3px;
-                    &:hover{
-                        background-color: #C0392B;
-                    }
-                    @include hoverCursor;
-
+                .area{
+                    margin-bottom: vh(50);
+                    border: 1px solid #e9e7ef;
+                    box-shadow: 3px 3px 3px #e9e7ef;
+                    border-radius: vh(5);
+                    box-sizing: border-box;
                 }
+               
             }
         }
     }
